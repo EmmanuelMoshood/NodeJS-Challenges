bbd75cddc585428f833cf06ed4ee3a0782a241e4
Emmanuel Moshood <emmanuelmoshood571@gmail.com>
2022-03-11 22:36:15 -0500

    Solve js problem with data issues

---
+++ DataTypes.js
@@ -0,0 +1,27 @@
+  // Declare second integer, double, and String variables.
+    let int;
+    let double;
+    let str;
+    
+    // Read and save an integer, double, and String to your variables.
+    
+    int = parseInt(readLine());
+    double = parseFloat(readLine());
+    str = readLine();
+
+    // Print the sum of both integer variables on a new line.
+    
+    console.log(int + i);
+    
+    
+    // Print the sum of the double variables on a new line.
+    
+     console.log((double + d).toFixed(1));
+    
+     
+    // Concatenate and print the String variables on a new line
+    console.log(s + str);
+    
+    // The 's' variable above should be printed first.
+
+}
